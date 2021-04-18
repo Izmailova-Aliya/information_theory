48c7ad03f21bc6a698fc6065f9d3efe88e5a073c
Zerdeli <58021111+Zerdeli@users.noreply.github.com>
2021-04-18 22:15:17 +0600

    Update ass3.py

--- ass3.py
+++ ass3.py
@@ -1,6 +1,14 @@
-print('hello')
-print('bye')
-print('Zerdeli')
-print('Vidno?')
-print('Da')
-print('Oyyy eeeee')
+count = dict()
+f = open("ass2.txt", "r").read()
+for x in list(f):
+    if x == ' ':
+        x = 'space'
+    if x == '\n':
+        x = 'new_line'
+    if x in count.keys():
+        count[x] += 1
+    else:
+        count[x] = 1
+count
+# count symbols/letters
+count = dict(sorted(count.items(), key=lambda item: item[1])) #sort dictionary by probability
