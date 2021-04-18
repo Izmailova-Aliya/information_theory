a91326ea9c1424d100c5814809a290d3e7e504c1
Aliya Izmailova <59693961+Izmailova-Aliya@users.noreply.github.com>
2021-04-18 22:16:00 +0600

    Update ass3.py

--- ass3.py
+++ ass3.py
@@ -12,3 +12,13 @@ for x in list(f):
 count
 # count symbols/letters
 count = dict(sorted(count.items(), key=lambda item: item[1])) #sort dictionary by probability
+
+
+for x in count.keys():
+    print(x + " - " + str(round(count[x] / len(f), 3)))
+class Node: #  for each letter creating node
+    letter = ""
+    num = 0
+    code = ""
+    left = None
+    right = None
